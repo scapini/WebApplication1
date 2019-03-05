750593ac950d3da53685e880e9ea06a0775361f0
matt <scapini@hotmail.com>
2019-03-05 12:26:11 +0000

    stuff

---
+++ WebApplication1/Contexts/PersonContext.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication1.Model;
+
+namespace WebApplication1.Contexts
+{
+    public class PersonContext : DbContext
+    {
+        public PersonContext()
+        {
+        }
+        public DbSet<Person> Persons { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=mydb;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+        }
+    }
+
+    public class PersonRepository : IDisposable
+    {
+        private PersonContext db = new PersonContext();
+
+        public IEnumerable<Person> GetAll()
+        {
+            return db.Persons;
+        }
+
+        public void Add(Person p)
+        {
+            db.Persons.Add(p);
+            db.SaveChanges();
+        }
+        public void Dispose(bool dis)
+        {
+            if (dis)
+            {
+                if (db != null)
+                {
+                    db.Dispose();
+                    db = null;
+                }
+            }
+        }
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
---
+++ WebApplication1/Controllers/PeopleController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Contexts;
+using WebApplication1.Model;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]")]
+    public class PeopleController : Controller
+    {
+
+        PersonRepository pr = new PersonRepository();
+        // GET: api/<controller>
+        [HttpGet]
+        public IEnumerable<Person> Get()
+        {
+            return pr.GetAll();
+        }
+
+        // GET api/<controller>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // GET api/<controller>/5
+        [Route("{id}/otherStuff")]
+        public string GetMore(int id)
+        {
+            return "other value";
+        }
+
+        // POST api/<controller>
+        [HttpPost]
+        public void Post([FromBody]Person per)
+        {
+            pr.Add(per);
+        }
+
+        // PUT api/<controller>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/<controller>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
